be4f143550ddfb1c0c494e03f1766fe02883d552
awu12 <tempatwisatabaru@gmail.com>
2018-09-22 05:58:24 +0700

    Update random.js

--- random.js
+++ random.js
@@ -23,7 +23,7 @@ var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/2018/09/7-common-myths-about-costum-erp.html"}
 if(x=="2"){xxx="/2018/09/top-5-reason-erp-implementations-fail.html"}
-if(x=="3"){xxx="/2018/2018/09/6-erp-trend-that-will-dominate-in-2018.html"}
+if(x=="3"){xxx="/2018/09/6-erp-trend-that-will-dominate-in-2018.html"}
 if(x=="4"){xxx="/2018/09/9-must-have-erp-features.html"}
 if(x=="5"){xxx="/2018/09/4-factors-to-consider-during-erp-system.html"}a_to_ve[i].href="http://www.erp-software-brands.online"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
